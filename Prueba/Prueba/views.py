a1bab43f42aab9c51e4efd6291c06b6d934d7bff
Sebastián Tapia <sebatapiareb@gmail.com>
2021-02-14 13:48:57 -0300

    Primer Commit

---
+++ Prueba/Prueba/views.py
@@ -0,0 +1,8 @@
+#Request: Realizar peticiones al servidor
+#Objeto HTTP Response: Es para enviar la respuesta usando el protocolo HTTP (80)
+from django.http import HttpResponse
+
+
+def welcome(request):
+    return HttpResponse("Welcome to my first page on Django")
+    
